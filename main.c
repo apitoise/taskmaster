521700ebb982b024ee8dd0370656783265d212d5
apitoise <arthur.pitoiset@outlook.fr>
2023-04-21 17:54:17 +0200

    proc prog proc procs prog

--- main.c
+++ main.c
@@ -41,6 +41,11 @@ typedef enum		restart_pol_e {
 }					restart_pol_t;
 
 typedef struct		proc_s {
+	pid_t			pid;
+	time_t			timestamp;
+}					proc_t;
+
+typedef struct		prog_s {
 	char			*name;
 	char			*cmd;
 	uint64_t		numprocs;
@@ -56,17 +61,11 @@ typedef struct		proc_s {
 	vec_t			*env;
 	char			*workingdir;
 	uint64_t		umask;
-	pid_t			pid;
-	time_t			timestamp;
-}					proc_t;
-
-typedef struct		proc_lst_s {
-	proc_t			data[STD_MAX];
-	uint64_t		sz;
-}					proc_lst_t;
+	vec_t			*procs;
+}					prog_t;
 
 typedef struct		clean_up_s {
-	proc_lst_t		*proc_lst;
+	vec_t			*prog_lst;
 	prompt_t		*prompt;
 	conf_t			*config;
 }					clean_up_t;
@@ -106,40 +105,40 @@ vec_t		*vec_unwrap(vec_t *vec, data_type_t type) {
 	return (new);
 }
 
-void		proc_print(proc_t *proc) {
+void		prog_print(prog_t *prog) {
 	uint64_t	i;
 
-	printf("name: \"%s\"\n", proc->name);
-	printf("cmd: \"%s\"\n", proc->cmd);
-	printf("numprocs: \"%lu\"\n", proc->numprocs);
-	printf("autostart: \"%s\"\n", g_map.bools[proc->autostart]);
-	printf("autorestart: \"%s\"\n", g_map.auto_r[proc->autorestart]);
+	printf("name: \"%s\"\n", prog->name);
+	printf("cmd: \"%s\"\n", prog->cmd);
+	printf("numprocs: \"%lu\"\n", prog->numprocs);
+	printf("autostart: \"%s\"\n", g_map.bools[prog->autostart]);
+	printf("autorestart: \"%s\"\n", g_map.auto_r[prog->autorestart]);
 	printf("exit_code: [ ");
-	for (i = 0; i < proc->exitcodes->sz; ++i)
-		printf("%lu%s", (uint64_t)proc->exitcodes->data[i],
-			i + 1 < proc->exitcodes->sz ? ", " : " ");
+	for (i = 0; i < prog->exitcodes->sz; ++i)
+		printf("%lu%s", (uint64_t)prog->exitcodes->data[i],
+			i + 1 < prog->exitcodes->sz ? ", " : " ");
 	printf("]\n");
-	printf("starttime: %lu\n", proc->starttime);
-	printf("startretries: %lu\n", proc->startretries);
+	printf("starttime: %lu\n", prog->starttime);
+	printf("startretries: %lu\n", prog->startretries);
 	printf("stopsignal: ");
 	for (i = 0; i < 7; ++i) {
-		if ((uint64_t)proc->stopsignal == g_map.sigs_v[i]) {
+		if ((uint64_t)prog->stopsignal == g_map.sigs_v[i]) {
 			printf("%s\n", g_map.sigs_s[i]);
 			break;
 		}
 	}
-	printf("stoptime: %lu\n", proc->stoptime);
-	printf("stdout: \"%s\"\n", proc->std_out);
-	printf("stderr: \"%s\"\n", proc->std_err);
-	printf("env: {%s", proc->env->sz ? "\n" : " ");
-	for (i = 0; i < proc->env->sz; ++i)
-		printf("  \"%s\"\n", (char *)proc->env->data[i]);
+	printf("stoptime: %lu\n", prog->stoptime);
+	printf("stdout: \"%s\"\n", prog->std_out);
+	printf("stderr: \"%s\"\n", prog->std_err);
+	printf("env: {%s", prog->env->sz ? "\n" : " ");
+	for (i = 0; i < prog->env->sz; ++i)
+		printf("  \"%s\"\n", (char *)prog->env->data[i]);
 	printf("}\n");
-	printf("workingdir: \"%s\"\n", proc->workingdir);
-	printf("umask: %s%lo\n", proc->umask ? "0" : "", proc->umask);
+	printf("workingdir: \"%s\"\n", prog->workingdir);
+	printf("umask: %s%lo\n", prog->umask ? "0" : "", prog->umask);
 }
 
-uint64_t	proc_map_str(uint64_t *out, char **in, char *str, uint64_t sz) {
+uint64_t	prog_map_str(uint64_t *out, char **in, char *str, uint64_t sz) {
 	uint64_t		i;
 
 	for (i = 0; i < sz; ++i)
@@ -148,73 +147,73 @@ uint64_t	proc_map_str(uint64_t *out, char **in, char *str, uint64_t sz) {
 	return (U_ERROR);
 }
 
-int			proc_init(proc_t *proc, char *name, dict_t *opts) {
+int			prog_init(prog_t *prog, char *name, dict_t *opts) {
 	char 		*str;
 	vec_t		*ex_cds, *env;
 
-	proc->name = name;
-	if (dic_get_unwrap(opts, "cmd", (void **)&proc->cmd, proc->name, DT_STR) < 0
-		|| dic_get_unwrap(opts, "numprocs", (void **)&proc->numprocs, (void *)1, DT_UNB) < 0
+	prog->name = name;
+	if (dic_get_unwrap(opts, "cmd", (void **)&prog->cmd, prog->name, DT_STR) < 0
+		|| dic_get_unwrap(opts, "numprocs", (void **)&prog->numprocs, (void *)1, DT_UNB) < 0
 		|| dic_get_unwrap(opts, "autostart", (void **)&str, "false", DT_STR) < 0
-		|| (proc->autostart = proc_map_str(NULL, g_map.bools, str, 2)) == U_ERROR
+		|| (prog->autostart = prog_map_str(NULL, g_map.bools, str, 2)) == U_ERROR
 		|| dic_get_unwrap(opts, "autorestart", (void **)&str, "unexpected", DT_STR) < 0
-		|| (proc->autorestart = proc_map_str(NULL, g_map.auto_r, str, 3)) == U_ERROR
+		|| (prog->autorestart = prog_map_str(NULL, g_map.auto_r, str, 3)) == U_ERROR
 		|| dic_get_unwrap(opts, "exitcodes", (void **)&ex_cds, NULL, DT_VEC) < 0
-		|| dic_get_unwrap(opts, "starttime", (void **)&proc->starttime, (void *)5, DT_UNB) < 0
-		|| dic_get_unwrap(opts, "startretries", (void **)&proc->startretries, NULL, DT_UNB) < 0
+		|| dic_get_unwrap(opts, "starttime", (void **)&prog->starttime, (void *)5, DT_UNB) < 0
+		|| dic_get_unwrap(opts, "startretries", (void **)&prog->startretries, NULL, DT_UNB) < 0
 		|| dic_get_unwrap(opts, "stopsignal", (void **)&str, "INT", DT_STR) < 0
-		|| (proc->stopsignal = proc_map_str(g_map.sigs_v, g_map.sigs_s, str, 7)) == U_ERROR
-		|| dic_get_unwrap(opts, "stoptime", (void **)&proc->stoptime, (void *)10, DT_UNB) < 0
-		|| dic_get_unwrap(opts, "stdout", (void **)&proc->std_out, "", DT_STR) < 0
-		|| dic_get_unwrap(opts, "stderr", (void **)&proc->std_err, "", DT_STR) < 0
+		|| (prog->stopsignal = prog_map_str(g_map.sigs_v, g_map.sigs_s, str, 7)) == U_ERROR
+		|| dic_get_unwrap(opts, "stoptime", (void **)&prog->stoptime, (void *)10, DT_UNB) < 0
+		|| dic_get_unwrap(opts, "stdout", (void **)&prog->std_out, "", DT_STR) < 0
+		|| dic_get_unwrap(opts, "stderr", (void **)&prog->std_err, "", DT_STR) < 0
 		|| dic_get_unwrap(opts, "env", (void **)&env, NULL, DT_VEC) < 0
-		|| dic_get_unwrap(opts, "workingdir", (void **)&proc->workingdir, "", DT_STR) < 0
-		|| dic_get_unwrap(opts, "umask", (void **)&proc->umask, NULL, DT_UNB)
-		|| !(proc->exitcodes = vec_unwrap(ex_cds, DT_UNB)))
+		|| dic_get_unwrap(opts, "workingdir", (void **)&prog->workingdir, "", DT_STR) < 0
+		|| dic_get_unwrap(opts, "umask", (void **)&prog->umask, NULL, DT_UNB)
+		|| !(prog->exitcodes = vec_unwrap(ex_cds, DT_UNB)))
 		return (-1);
-	if (!proc->exitcodes->sz)
-		vec_push_back(proc->exitcodes, 0);
-	if (!(proc->env = vec_unwrap(env, DT_STR))) {
-		vec_free(proc->exitcodes);
+	if (!prog->exitcodes->sz)
+		vec_push_back(prog->exitcodes, 0);
+	if (!(prog->env = vec_unwrap(env, DT_STR))) {
+		vec_free(prog->exitcodes);
 		return (-1);
 	}
-	vec_push_back(proc->env, NULL);
+	vec_push_back(prog->env, NULL);
 	return (0);
 }
 
-void		proc_free(proc_t *proc) {
-	vec_free(proc->exitcodes);
-	vec_free(proc->env);
+void		prog_free(prog_t *prog) {
+	vec_free(prog->exitcodes);
+	vec_free(prog->env);
 }
 
-int			proc_lst_init(proc_lst_t *proc_lst, conf_t *conf) {
+int			prog_lst_init(vec_t *prog_lst, conf_t *conf) {
 	dict_t		*root, *opts;
 	uint64_t	i;
 
 	if (node_unwrap(conf->root, (void **)&root, DT_DIC)
 		|| root->keys->sz > STD_MAX)
 		return (-1);
-	proc_lst->sz = 0;
-	for (i = 0; i < root->keys->sz; ++i, ++proc_lst->sz) {
+	prog_lst->sz = 0;
+	for (i = 0; i < root->keys->sz; ++i, ++prog_lst->sz) {
 		if (node_unwrap(root->values->data[i], (void **)&opts, DT_DIC)
-			|| proc_init(&proc_lst->data[i], (char *)root->keys->data[i], opts))
+			|| prog_init((prog_t *)&prog_lst->data[i], (char *)root->keys->data[i], opts))
 			return (-1);
 	}
 	return (0);
 }
 
-void		proc_lst_free(proc_lst_t *proc_lst) {
+void		prog_lst_free(vec_t *prog_lst) {
 	uint64_t		i;
 
-	for (i = 0; i < proc_lst->sz; ++i)
-		proc_free(&proc_lst->data[i]);
+	for (i = 0; i < prog_lst->sz; ++i)
+		prog_free((prog_t *)&prog_lst->data[i]);
 }
 
 void	clean_exit(char *error, int ret) {
 	if (error)
 		fprintf(stderr, "Error: %s\n", error);
-	if (g_clean_up.proc_lst)
-		proc_lst_free(g_clean_up.proc_lst);
+	if (g_clean_up.prog_lst)
+		prog_lst_free(g_clean_up.prog_lst);
 	if (g_clean_up.prompt)
 		prompt_fini(g_clean_up.prompt);
 	exit(ret);
@@ -253,7 +252,7 @@ static int	proc_redirect(int old_fd, char *new_path) {
 	return (0);
 }
 
-int			proc_run(proc_t *proc) {
+int			proc_run(prog_t *prog, proc_t *proc) {
 	pid_t	pid;
 	char	*args[STD_MAX];
 
@@ -264,26 +263,28 @@ int			proc_run(proc_t *proc) {
 		if (time(&proc->timestamp) == -1)
 			return (-1);
 	} else if (!pid) {
-		if (proc_redirect(STDOUT_FILENO, proc->std_out)
-			|| proc_redirect(STDERR_FILENO, proc->std_err)
-			|| str_split(proc->cmd, args, STD_MAX)
-			|| execve(proc->cmd, args, (char **)proc->env->data))
+		if (proc_redirect(STDOUT_FILENO, prog->std_out)
+			|| proc_redirect(STDERR_FILENO, prog->std_err)
+			|| str_split(prog->cmd, args, STD_MAX)
+			|| execve(prog->cmd, args, (char **)prog->env->data))
 			exit(1);
 	}
 	return (0);
 }
 
-int			proc_lst_run(proc_lst_t *proc_lst) {
+int			prog_lst_run(vec_t *prog_lst) {
 	uint64_t	i, procs_nb;
 	int			ret = 0;
+	prog_t		*prog;
 
-	for (i = 0; i < proc_lst->sz; ++i) {
-		if (proc_lst->data[i].autostart) {
-			for (procs_nb = 0; procs_nb < proc_lst->data[i].numprocs; ++procs_nb) {
-				if (proc_run(&proc_lst->data[i]) == -1) {
+	for (i = 0; i < prog_lst->sz; ++i) {
+		prog = (prog_t *)prog_lst->data[i];
+		if (prog->autostart) {
+			for (procs_nb = 0; procs_nb < prog->numprocs; ++procs_nb) {
+				if (proc_run(prog, (proc_t *)prog->procs->data[i])) {
 					ret = -1;
 					fprintf(stderr, "Error: %s %lu/%lu can't be run.\n"
-						, proc_lst->data[i].name, procs_nb, proc_lst->data[i].numprocs);
+						, prog->name, procs_nb, prog->numprocs);
 				}
 			}
 		}
@@ -298,7 +299,7 @@ int			proc_lst_run(proc_lst_t *proc_lst) {
 int			main(int ac, char **av) {
 	prompt_t		prompt;
 	cmd_t			cmd;
-	proc_lst_t		proc_lst = { 0 };
+	vec_t		prog_lst = { 0 };
 	action_t		action;
 	conf_t			conf;
 
@@ -307,10 +308,10 @@ int			main(int ac, char **av) {
 	if (config_init(&conf, av[1]))
 		clean_exit("Can't load config file", 2);
 	g_clean_up.config = &conf;
-	if (proc_lst_init(&proc_lst, &conf))
+	if (prog_lst_init(&prog_lst, &conf))
 		clean_exit("Can't init program's list", 3);
-	g_clean_up.proc_lst = &proc_lst;
-	if (proc_lst_run(&proc_lst) == -1)
+	g_clean_up.prog_lst = &prog_lst;
+	if (prog_lst_run(&prog_lst) == -1)
 		clean_exit("Fork failed", 4);
 	if (prompt_init(&prompt, "> "))
 		clean_exit("Can init prompt", 5);
